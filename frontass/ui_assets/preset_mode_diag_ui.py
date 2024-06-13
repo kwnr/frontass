65ea4c7d2f49e119aa09582ee6eaa8d065a6ff7a
hyeokbeom <khb100100@naver.com>
2024-06-13 13:22:14 +0900

    spinbox range adjusted

--- frontass/ui_assets/preset_mode_diag_ui.py
+++ frontass/ui_assets/preset_mode_diag_ui.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'preset_mode_diag.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.2.4
+## Created by: Qt User Interface Compiler version 6.7.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -28,7 +28,7 @@ class Ui_Dialog(object):
         Dialog.resize(565, 306)
         self.formLayoutWidget = QWidget(Dialog)
         self.formLayoutWidget.setObjectName(u"formLayoutWidget")
-        self.formLayoutWidget.setGeometry(QRect(20, 20, 101, 261))
+        self.formLayoutWidget.setGeometry(QRect(20, 20, 103, 261))
         self.formLayout = QFormLayout(self.formLayoutWidget)
         self.formLayout.setObjectName(u"formLayout")
         self.formLayout.setContentsMargins(0, 0, 0, 0)
@@ -39,8 +39,9 @@ class Ui_Dialog(object):
 
         self.l1DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget)
         self.l1DoubleSpinBox.setObjectName(u"l1DoubleSpinBox")
-        self.l1DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
-        self.l1DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.l1DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+        self.l1DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.l1DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout.setWidget(0, QFormLayout.FieldRole, self.l1DoubleSpinBox)
 
@@ -51,8 +52,9 @@ class Ui_Dialog(object):
 
         self.l2DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget)
         self.l2DoubleSpinBox.setObjectName(u"l2DoubleSpinBox")
-        self.l2DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
-        self.l2DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.l2DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+        self.l2DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.l2DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout.setWidget(1, QFormLayout.FieldRole, self.l2DoubleSpinBox)
 
@@ -63,8 +65,9 @@ class Ui_Dialog(object):
 
         self.l3DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget)
         self.l3DoubleSpinBox.setObjectName(u"l3DoubleSpinBox")
-        self.l3DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
-        self.l3DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.l3DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+        self.l3DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.l3DoubleSpinBox.setMaximum(999.990000000000009)
         self.l3DoubleSpinBox.setValue(20.000000000000000)
 
         self.formLayout.setWidget(2, QFormLayout.FieldRole, self.l3DoubleSpinBox)
@@ -76,8 +79,9 @@ class Ui_Dialog(object):
 
         self.l4DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget)
         self.l4DoubleSpinBox.setObjectName(u"l4DoubleSpinBox")
-        self.l4DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
-        self.l4DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.l4DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+        self.l4DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.l4DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout.setWidget(3, QFormLayout.FieldRole, self.l4DoubleSpinBox)
 
@@ -88,8 +92,9 @@ class Ui_Dialog(object):
 
         self.l5DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget)
         self.l5DoubleSpinBox.setObjectName(u"l5DoubleSpinBox")
-        self.l5DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
-        self.l5DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.l5DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+        self.l5DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.l5DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout.setWidget(4, QFormLayout.FieldRole, self.l5DoubleSpinBox)
 
@@ -100,8 +105,9 @@ class Ui_Dialog(object):
 
         self.l6DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget)
         self.l6DoubleSpinBox.setObjectName(u"l6DoubleSpinBox")
-        self.l6DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
-        self.l6DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.l6DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+        self.l6DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.l6DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout.setWidget(5, QFormLayout.FieldRole, self.l6DoubleSpinBox)
 
@@ -112,8 +118,9 @@ class Ui_Dialog(object):
 
         self.l7DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget)
         self.l7DoubleSpinBox.setObjectName(u"l7DoubleSpinBox")
-        self.l7DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
-        self.l7DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.l7DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+        self.l7DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.l7DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout.setWidget(6, QFormLayout.FieldRole, self.l7DoubleSpinBox)
 
@@ -124,14 +131,15 @@ class Ui_Dialog(object):
 
         self.l8DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget)
         self.l8DoubleSpinBox.setObjectName(u"l8DoubleSpinBox")
-        self.l8DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
-        self.l8DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.l8DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+        self.l8DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.l8DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout.setWidget(7, QFormLayout.FieldRole, self.l8DoubleSpinBox)
 
         self.formLayoutWidget_2 = QWidget(Dialog)
         self.formLayoutWidget_2.setObjectName(u"formLayoutWidget_2")
-        self.formLayoutWidget_2.setGeometry(QRect(140, 20, 101, 261))
+        self.formLayoutWidget_2.setGeometry(QRect(140, 20, 104, 261))
         self.formLayout_2 = QFormLayout(self.formLayoutWidget_2)
         self.formLayout_2.setObjectName(u"formLayout_2")
         self.formLayout_2.setContentsMargins(0, 0, 0, 0)
@@ -142,8 +150,9 @@ class Ui_Dialog(object):
 
         self.r1DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget_2)
         self.r1DoubleSpinBox.setObjectName(u"r1DoubleSpinBox")
-        self.r1DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTop|Qt.AlignTrailing)
-        self.r1DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.r1DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTop|Qt.AlignmentFlag.AlignTrailing)
+        self.r1DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.r1DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout_2.setWidget(0, QFormLayout.FieldRole, self.r1DoubleSpinBox)
 
@@ -154,8 +163,9 @@ class Ui_Dialog(object):
 
         self.r2DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget_2)
         self.r2DoubleSpinBox.setObjectName(u"r2DoubleSpinBox")
-        self.r2DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTop|Qt.AlignTrailing)
-        self.r2DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.r2DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTop|Qt.AlignmentFlag.AlignTrailing)
+        self.r2DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.r2DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout_2.setWidget(1, QFormLayout.FieldRole, self.r2DoubleSpinBox)
 
@@ -166,8 +176,9 @@ class Ui_Dialog(object):
 
         self.r3DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget_2)
         self.r3DoubleSpinBox.setObjectName(u"r3DoubleSpinBox")
-        self.r3DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTop|Qt.AlignTrailing)
-        self.r3DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.r3DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTop|Qt.AlignmentFlag.AlignTrailing)
+        self.r3DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.r3DoubleSpinBox.setMaximum(999.990000000000009)
         self.r3DoubleSpinBox.setValue(20.000000000000000)
 
         self.formLayout_2.setWidget(2, QFormLayout.FieldRole, self.r3DoubleSpinBox)
@@ -179,8 +190,9 @@ class Ui_Dialog(object):
 
         self.r4DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget_2)
         self.r4DoubleSpinBox.setObjectName(u"r4DoubleSpinBox")
-        self.r4DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTop|Qt.AlignTrailing)
-        self.r4DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.r4DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTop|Qt.AlignmentFlag.AlignTrailing)
+        self.r4DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.r4DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout_2.setWidget(3, QFormLayout.FieldRole, self.r4DoubleSpinBox)
 
@@ -191,8 +203,9 @@ class Ui_Dialog(object):
 
         self.r5DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget_2)
         self.r5DoubleSpinBox.setObjectName(u"r5DoubleSpinBox")
-        self.r5DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTop|Qt.AlignTrailing)
-        self.r5DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.r5DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTop|Qt.AlignmentFlag.AlignTrailing)
+        self.r5DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.r5DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout_2.setWidget(4, QFormLayout.FieldRole, self.r5DoubleSpinBox)
 
@@ -203,8 +216,9 @@ class Ui_Dialog(object):
 
         self.r6DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget_2)
         self.r6DoubleSpinBox.setObjectName(u"r6DoubleSpinBox")
-        self.r6DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTop|Qt.AlignTrailing)
-        self.r6DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.r6DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTop|Qt.AlignmentFlag.AlignTrailing)
+        self.r6DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.r6DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout_2.setWidget(5, QFormLayout.FieldRole, self.r6DoubleSpinBox)
 
@@ -215,8 +229,9 @@ class Ui_Dialog(object):
 
         self.r7DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget_2)
         self.r7DoubleSpinBox.setObjectName(u"r7DoubleSpinBox")
-        self.r7DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTop|Qt.AlignTrailing)
-        self.r7DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.r7DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTop|Qt.AlignmentFlag.AlignTrailing)
+        self.r7DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.r7DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout_2.setWidget(6, QFormLayout.FieldRole, self.r7DoubleSpinBox)
 
@@ -227,8 +242,9 @@ class Ui_Dialog(object):
 
         self.r8DoubleSpinBox = QDoubleSpinBox(self.formLayoutWidget_2)
         self.r8DoubleSpinBox.setObjectName(u"r8DoubleSpinBox")
-        self.r8DoubleSpinBox.setAlignment(Qt.AlignRight|Qt.AlignTop|Qt.AlignTrailing)
-        self.r8DoubleSpinBox.setMinimum(-99.989999999999995)
+        self.r8DoubleSpinBox.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTop|Qt.AlignmentFlag.AlignTrailing)
+        self.r8DoubleSpinBox.setMinimum(-999.990000000000009)
+        self.r8DoubleSpinBox.setMaximum(999.990000000000009)
 
         self.formLayout_2.setWidget(7, QFormLayout.FieldRole, self.r8DoubleSpinBox)
 
@@ -238,7 +254,7 @@ class Ui_Dialog(object):
         self.buttonBox = QDialogButtonBox(Dialog)
         self.buttonBox.setObjectName(u"buttonBox")
         self.buttonBox.setGeometry(QRect(260, 240, 291, 30))
-        self.buttonBox.setStandardButtons(QDialogButtonBox.Apply|QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Apply|QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
         self.buttonBox.setCenterButtons(False)
         self.toolButton = QToolButton(Dialog)
         self.toolButton.setObjectName(u"toolButton")
